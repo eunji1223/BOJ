c99b0d0cbc1da7fd5106e5b8ad97e105b852596b
eunji1223 <djer12@naver.com>
2022-02-16 13:32:55 +0900

    complete

---
+++ 17단계. 정수론 및 조합론/10.9375.py
@@ -0,0 +1,11 @@
+case=int(input())
+for i in range(case):
+    num=int(input())
+    wearlist=[]
+    wearlistc=[]
+    count=num
+    for j in range(num):
+        name=list(input().split())
+        if name[-1] in wearlist:
+            
+        
