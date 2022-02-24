e28e3b5a415715abc8c103e904f53269861dc18e
eunji1223 <djer12@naver.com>
2022-02-24 12:54:52 +0900

    complete

--- 15단계. 동적 계획법 1/15.1912.py
+++ 15단계. 동적 계획법 1/15.1912.py
@@ -1,5 +1,6 @@
 
-"""최대구간합(간단한 방법) --> 따라서 시간초과(O(n^2))
+"""
+#최대구간합(간단한 방법) --> 따라서 시간초과(O(n^2))
 def max_sum(A):
     P=[0]
     sum=0
@@ -16,4 +17,31 @@ num=int(input())
 numlist=list(map(int,input().split()))
 sol=max_sum(numlist)
 print(sol)
+------------------------------------------------------
+#divide&conquer 방식 --> 시간초과
+def max_sum(A,left,right):
+    if left>=right: return A[left]
+    m=(left+right)//2
+    L=max_sum(A,left,m)
+    R=max_sum(A,m+1,right)
+    M=A[m]+A[m+1]
+    Ml=Mr=-99999
+    s=0
+    for i in range(m,-1,-1):
+        s+=A[i]
+        if s>Ml:
+            Ml=s
+    s=0
+    for j in range(m+1,right+1):
+        s+=A[j]
+        if s>Mr:
+            Mr=s
+    M=Mr+Ml
+    return max(L,M,R)
+
+num=int(input())
+numlist=list(map(int,input().split()))
+sol=max_sum(numlist,0,num-1)
+print(sol)
 """
+
