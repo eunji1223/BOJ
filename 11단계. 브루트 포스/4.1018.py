148288e35905e0d1f1012fba2c4ef557ea1965b6
eunji1223 <djer12@naver.com>
2022-02-24 12:30:20 +0900

    complete

--- 11단계. 브루트 포스/4.1018.py
+++ 11단계. 브루트 포스/4.1018.py
@@ -1,4 +1,30 @@
 N,M=map(int,input().split())
+board=list()
+for i in range(N):
+    board.append(input())
+repair=list()
+
+for i in range(N-7):
+    for j in range(M-7):
+        first_W=0
+        first_B=0
+        for k in range(i,i+8):
+            for l in range(j,j+8):
+                if(k+l)%2==0:
+                    if board[k][l]!='W':
+                        first_W+=1
+                    if board[k][l]!='B':
+                        first_B+=1
+                else:
+                    if board[k][l]!='B':
+                        first_W+=1
+                    if board[k][l]!='W':
+                        first_B+=1
+        repair.append(first_W)
+        repair.append(first_B)
+print(min(repair))
+"""
+N,M=map(int,input().split())
 chess=[] #체스판
 first=[] #첫 시작
 count=0
@@ -15,4 +41,5 @@ for j in range(len(chess)-1):
             if chess[j][k]!=first[j]:continue
             else:count+=1
 print(count)
+"""
         
