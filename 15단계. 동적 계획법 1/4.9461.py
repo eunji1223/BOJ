b1df43babc0e339f82da0200d25a1b1ae0e5f7d9
eunji1223 <djer12@naver.com>
2022-03-01 22:59:23 +0900

    complete

---
+++ 15단계. 동적 계획법 1/4.9461.py
@@ -0,0 +1,12 @@
+numlist=[1,1,1]
+def find(num):
+    if numlist[num]:
+        return numlist[num]
+    else:
+        numlist.append(numlist[num-2]+numlist[num-3])
+T=int(input())
+for i in range(T):
+    num=int(input())
+    print(find(num))
+
+    
