7da1d368f9c9cf4d5bc0e1b047a3563d0aaa6e82
eunji1223 <djer12@naver.com>
2022-03-04 17:11:17 +0900

    complete

--- 15단계. 동적 계획법 1/8.1463.py
+++ 15단계. 동적 계획법 1/8.1463.py
@@ -1,3 +1,13 @@
+n=int(input())
+dp[0]*(n+1)
+for i in range(2,n+1):
+    dp[i]=dp[i-1]+1
+    if i%3==0:
+        dp[i]=min(dp[i],dp[i//3]+1)
+    if i%2==0:
+        dp[i]=min(dp[i],dp[i//2]+1)
+print(dp[n])
+"""
 def find(N,count):
     if N==1:
         print(count)
@@ -11,3 +21,21 @@ def find(N,count):
 
 N=int(input())
 result=find(N,0)
+
+
+def one(N):
+    return N-1
+def two(N):
+    if N%2==0:return N//2
+    else:return None
+def three(N):
+    if N%3==0:return N//3
+    else:return None
+def repeat(N,count):
+    
+    if two(N)!=None:
+N=int(input())
+Nli=[]
+
+"""
+    
