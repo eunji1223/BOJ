210f775778fb7d4926d132b3f1edc656d731e5f8
eunji1223 <djer12@naver.com>
2022-03-08 09:44:11 +0900

    complete

--- 15단계. 동적 계획법 1/11.11053.py
+++ 15단계. 동적 계획법 1/11.11053.py
@@ -1,13 +1,15 @@
-L=0
-for i in range(1,n-1):
-    low=1
-    high=L
-    while low<=high:
-        mid=(low+high)//2
-        if A[M[mid]]<A[i]:low=mid+1
-        else:high=mid-1
-    newL=low
-    M[newL]=i
-    parent[k]=M[newL-1]
-    L=max(L,newL)
-    
+#입력 데이터가 1000개 --> 시간제한 1초, 관습적으로 1000000000개 정도의 연산하면 1초
+#이론적으로 O(n^3)알고리즘까지 인정
+
+N=int(input())
+A=list(map(int,input().split()))
+D=[0]*(N+1)
+maxN=0
+for i in range(1,N):
+    for j in range(0,i):
+        if(A[j]<=A[i]):
+            if D[j]+1>D[i]:
+                D[i]=D[j]+1
+            else: D[i]=D[i]
+            if maxN<D[i]: maxN=D[i]
+print(maxN)
