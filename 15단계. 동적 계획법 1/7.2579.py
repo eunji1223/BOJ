b0a1f19d9dcbae3b70ad9c7b8c8eaf26a4da8811
eunji1223 <djer12@naver.com>
2022-03-09 22:43:43 +0900

    complete

--- 15단계. 동적 계획법 1/7.2579.py
+++ 15단계. 동적 계획법 1/7.2579.py
@@ -1,6 +1,11 @@
-n=int(input())
-score=[]
-for i in range(n):
-    score.append(int(input()))
-for j in range()
-    
+N=int(input())
+S=[0 for _ in range(301)]
+D=[0 for _ in range(301)]
+for i in range(N):
+    S[i]=int(input())
+D[0]=S[0]
+D[1]=S[0]+S[1]
+D[2]=max(S[1]+S[2],S[0]+S[2])
+for i in range(3,N):
+    D[i]=max(D[i-3]+S[i-1]+S[i],D[i-2]+S[i])
+print(D[N-1])
