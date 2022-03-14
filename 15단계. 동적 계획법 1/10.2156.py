d20c9d90bf02db50c1fead797664795d460b2de2
eunji1223 <djer12@naver.com>
2022-03-14 11:43:50 +0900

    complete

--- 15단계. 동적 계획법 1/10.2156.py
+++ 15단계. 동적 계획법 1/10.2156.py
@@ -1,13 +1,13 @@
-A=[]
-DP=[]
 N=int(input())
+A=[]
+DP=[0]*N
 for i in range(N):
     A.append(int(input()))
 if N>1:
-    DP.append(A[0])
-    DP.append(A[0]+A[1])
+    DP[0]=A[0]
+    DP[1]=A[0]+A[1]
 if N>2:
-    DP.append(max(A[2]+A[1],A[2]+A[0],DP[1]))
+    DP[2]=max(A[2]+A[1],A[2]+A[0],DP[1])
 for i in range(3,N):
-    DP.append(max(DP[i-1],DP[i-3]+A[i-1]+A[i],DP[i-2]+A[i]))
+    DP[i]=max(DP[i-1],DP[i-3]+A[i-1]+A[i],DP[i-2]+A[i])
 print(DP[N-1])
