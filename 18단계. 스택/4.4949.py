c437f37060aa76c9b654d7f39b538304134d52ba
eunji1223 <djer12@naver.com>
2022-03-15 22:53:24 +0900

    complete

--- 18단계. 스택/4.4949.py
+++ 18단계. 스택/4.4949.py
@@ -1,3 +1,4 @@
+"""
 S=input()
 while S!='.':
     SL=S.replace(" ","")
@@ -13,6 +14,27 @@ while S!='.':
     if sum1==0 and sum2==0:print('yes')
     else: print('no')
     S=input()
+"""
+
+lines=input()
+while lines!='.':
+    stack=[]
+    for t in lines:
+        if t in '([':
+            stack.append(t)
+        elif t==']':
+            if not stack or stack.pop()!='[':
+                print('no')
+                break
+        elif t==')':
+            if not stack or stack.pop()!='(':
+                print('no')
+                break
+        elif t=='.':
+            if stack: print('no')
+            else: print('yes')
+    lines=input()
+    
         
     
     
