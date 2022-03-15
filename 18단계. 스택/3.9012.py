ecc2ba8a3e440023775c6be1b176b67083149fcf
eunji1223 <djer12@naver.com>
2022-03-15 22:17:43 +0900

    complete

--- 18단계. 스택/3.9012.py
+++ 18단계. 스택/3.9012.py
@@ -4,15 +4,11 @@ for i in range(N):
     IL=list(I)
     sum=0
     for i in IL:
-        if i=="(":
-            sum+=1
-        elif i==")":
-            sum-=1
+        if i=="(":sum+=1
+        elif i==")":sum-=1
         if sum<0:
             print('NO')
             break
-    if sum>0:
-        print('NO')
-    elif sum==0:
-        print('YES')
+    if sum>0:print('NO')
+    elif sum==0:print('YES')
         
