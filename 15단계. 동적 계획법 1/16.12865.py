482c77e3ebe8e9746e6586e24f934e1f7bad8b6a
eunji1223 <djer12@naver.com>
2022-03-16 22:50:06 +0900

    complete

---
+++ 15단계. 동적 계획법 1/16.12865.py
@@ -0,0 +1,13 @@
+N,K=map(int,input().split())
+napsack=[]
+for i in range(N):
+    napsack.append(list(map(int(int,input().split()))))
+
+def Knapsack(i,T):
+    if i>=n or T<=0:
+        print(x)
+        return
+
+    x[i]=1
+    if s+size[i]<=K:
+        B
