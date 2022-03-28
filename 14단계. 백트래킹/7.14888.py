417688838f3f3ce670fdd4541fca1a2da55ff6cd
eunji1223 <djer12@naver.com>
2022-03-28 22:32:34 +0900

    complete

--- 14단계. 백트래킹/7.14888.py
+++ 14단계. 백트래킹/7.14888.py
@@ -1,3 +1,23 @@
 N=int(input())
-A=list(map(int,input().split()))
-plus, minus, multip, div=map(int,input().split())
+AN=list(map(int,input().split()))
+p,m,mu,d=map(int,input().split())
+maxN,minN=-100000000,100000000
+
+def sol(A,i,p,m,mu,d):
+    global maxN,minN
+    if i==N:
+        maxN=max(maxN,A)
+        minN=min(minN,A)
+        return
+    if p>0:
+        sol(A+AN[i],i+1,p-1,m,mu,d)
+    if m>0:
+        sol(A-AN[i],i+1,p,m-1,mu,d)
+    if mu>0:
+        sol(A*AN[i],i+1,p,m,mu-1,d)
+    if d>0:
+        sol(int(A/AN[i]),i+1,p,m,mu,d-1)
+
+sol(AN[0],1,p,m,mu,d)
+print(maxN)
+print(minN)
