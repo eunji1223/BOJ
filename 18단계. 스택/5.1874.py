c48bd054b57cf468137fd034443cd344423504ec
eunji1223 <djer12@naver.com>
2022-04-05 22:42:33 +0900

    complete

--- 18단계. 스택/5.1874.py
+++ 18단계. 스택/5.1874.py
@@ -1,4 +1,16 @@
 n=int(input())
+curr=1
+stack,ans=[],[]
 for i in range(n):
     N=int(input())
-    for j in r
+    while curr<=N:
+        stack.append(curr)
+        ans.append('+')
+        curr+=1
+    if stack[-1]==N:
+        ans.append('-')
+        stack.pop()
+if not stack:
+    print('\n'.join(ans))
+else:
+    print('No')
