0f33fe3bd233f2789db4359e07906180b06a760e
eunji1223 <djer12@naver.com>
2022-04-05 22:45:53 +0900

    complete

--- 18단계. 스택/5.1874.py
+++ 18단계. 스택/5.1874.py
@@ -5,12 +5,12 @@ for i in range(n):
     N=int(input())
     while curr<=N: #현재 수가 입력받은 수와 같아질 때까지
         stack.append(curr) #스택에 숫자를 넣음
-        ans.append('+') #숫자 추가된 갯수
+        ans.append('+') #숫자 추가됨
         curr+=1
-    if stack[-1]==N:
-        ans.append('-')
-        stack.pop()
-if not stack:
+    if stack[-1]==N: #마지막 값과 같을 경우
+        ans.append('-') #숫자 감소됨
+        stack.pop() #스택에서 숫자를 뺌
+if not stack: #최종 결과 값 출력
     print('\n'.join(ans))
 else:
     print('No')
