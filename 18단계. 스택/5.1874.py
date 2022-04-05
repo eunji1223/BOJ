7ba5516e21ed81a24bc672c8d67b17059bee9cc8
eunji1223 <djer12@naver.com>
2022-04-05 22:44:41 +0900

    complete

--- 18단계. 스택/5.1874.py
+++ 18단계. 스택/5.1874.py
@@ -3,9 +3,9 @@ curr=1 #현재 입력(수)
 stack,ans=[],[]
 for i in range(n):
     N=int(input())
-    while curr<=N:
-        stack.append(curr)
-        ans.append('+')
+    while curr<=N: #현재 수가 입력받은 수와 같아질 때까지
+        stack.append(curr) #스택에 숫자를 넣음
+        ans.append('+') #숫자 추가된 갯수
         curr+=1
     if stack[-1]==N:
         ans.append('-')
