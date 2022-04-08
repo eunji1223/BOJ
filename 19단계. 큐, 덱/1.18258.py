70037f4b829405d717dba9f14f47a9cb21ebcbea
eunji1223 <djer12@naver.com>
2022-04-08 23:16:26 +0900

    complete

--- 19단계. 큐, 덱/1.18258.py
+++ 19단계. 큐, 덱/1.18258.py
@@ -1,1 +1,20 @@
+queue=[]
+N=int(input())
+for i in range(N):
+    order=input()
+    if order in 'push':
+        order,a=order.split()
+        queue.append(a)
+    elif order=='pop':
 
+    elif order=='size':
+        print(len(queue))
+    elif order=='empty':
+        if len(queue)==0:
+            print('1')
+        else:
+            print('0')
+    elif order=='front':
+        print(queue[0])
+    elif order=='back':
+        print(queue[-1])
