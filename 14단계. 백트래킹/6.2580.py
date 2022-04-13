b32a6a77dcb859a4b33986447836106b3934b130
eunji1223 <djer12@naver.com>
2022-04-13 13:03:00 +0900

    complete

--- 14단계. 백트래킹/6.2580.py
+++ 14단계. 백트래킹/6.2580.py
@@ -1,5 +1,5 @@
-sdk=[list(map(int,input().split())) for _ in range(9]
-zero_loc=[(i,j) for i in range(9) for j in range(9) if board[i][j]==0]
+sdk=[list(map(int,input().split())) for _ in range(9)]
+zero_loc=[(i,j) for i in range(9) for j in range(9) if sdk[i][j]==0]
 
 def findN(y,x):
     number=[i+1 for i in range(9)]
