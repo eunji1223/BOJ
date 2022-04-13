6f576bc4de7f9b792caebce0e13b2761f9bd07e8
eunji1223 <djer12@naver.com>
2022-04-13 12:57:22 +0900

    complete

--- 14단계. 백트래킹/6.2580.py
+++ 14단계. 백트래킹/6.2580.py
@@ -1,11 +1,20 @@
 sdk=[list(map(int,input().split())) for _ in range(9]
 zero_loc=[(i,j) for i in range(9) for j in range(9) if board[i][j]==0]
 
-def solve(y,x):
+def findN(y,x):
     number=[i+1 for i in range(9)]
     for i in range(9):
         if sdk[y][i] in number:
             number.remove(sdk[y][i])
         if sdk[i][x] in number:
             number.remove(sdk[i][x])
+    y=y//3
+    x=x//3
+    for i in range(y*3,(y+1)*3):
+        for j in range(x*3,(x+1)*3):
+            if sdk[i][j] in number:
+                numbers.remove(board[i][j])
+    return number
+
+def solve(count):
     
