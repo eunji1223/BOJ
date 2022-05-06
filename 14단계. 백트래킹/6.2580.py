123c1e342f5421dd946f474a043a707ab041243f
eunji1223 <djer12@naver.com>
2022-05-06 15:42:13 +0900

    complete

--- 14단계. 백트래킹/6.2580.py
+++ 14단계. 백트래킹/6.2580.py
@@ -27,6 +27,8 @@ def solve(i):
         solve(i+1)
         sdk[r][c]=0
 solve(0)
+
+
 """
 sdk=[list(map(int,input().split())) for _ in range(9)]
 zero_loc=[(i,j) for i in range(9) for j in range(9) if sdk[i][j]==0]
