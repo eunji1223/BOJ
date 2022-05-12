dd590da479bfe1d490be86bb9905b973c74bb04d
eunji1223 <djer12@naver.com>
2022-05-12 23:47:07 +0900

    complete

--- 13단계. 기하 1/4.3034.py
+++ 13단계. 기하 1/4.3034.py
@@ -1,7 +1,8 @@
+import math
 N,W,H=map(int,input().split())
 for i in range(N):
     t=int(input())
-    if t<W or t<H or (t^2)<(W^2+H^2):
+    if t<=W or t<=H or t<=math.sqrt(W**2+H**2):
         print("DA")
     else:
         print("NE")
