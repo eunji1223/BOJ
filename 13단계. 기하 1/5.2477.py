caa3fa526758b1a458779ce2fe0cfda36f0cf7c6
eunji1223 <djer12@naver.com>
2022-05-14 16:49:49 +0900

    complete

--- 13단계. 기하 1/5.2477.py
+++ 13단계. 기하 1/5.2477.py
@@ -1,15 +1,18 @@
 k=int(input())
 NL=[]
 Br=Bc=ridx=cidx=0
-for i in range(k):
+for i in range(1,k):
     n,l=map(int,input().split())
     if i%2==0:
         if l>Br:
             Br=l
             ridx=i
+            print("실행")
     else:
         if l>Bc:
             Bc=l
             cidx=0
     NL.append(l)
-print(7*(Br*Bc-NL[ridx+3]*NL[cidx+3]))
+print(ridx,cidx)
+result=Br*Bc-NL[ridx+3]*NL[cidx+3]
+print(7*result)
