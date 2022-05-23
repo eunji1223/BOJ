2dd38894cab0817eed38393b8d8448401756263a
eunji1223 <djer12@naver.com>
2022-05-24 00:14:43 +0900

    complete

--- 12단계. 집합과 맵/3.1620.py
+++ 12단계. 집합과 맵/3.1620.py
@@ -1,9 +1,10 @@
-N,M=map(int,input().split())
+import sys
+N,M=map(int,sys.stdin.readline().split())
 Doc={}
 for i in range(1,N+1):
-    name=input()
+    name=sys.stdin.readline().strip()
     Doc[str(i)]=name
     Doc[name]=i
 for j in range(M):
-    Q=input()
+    Q=sys.stdin.readline().strip()
     print(Doc[Q])
