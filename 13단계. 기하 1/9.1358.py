c81e8a732cff891b2917989cc7b8e21a9c4a3e48
eunji1223 <djer12@naver.com>
2022-05-23 23:47:49 +0900

    complete

--- 13단계. 기하 1/9.1358.py
+++ 13단계. 기하 1/9.1358.py
@@ -1,3 +1,4 @@
+import math
 W,H,X,Y,P=map(int,input().split())
 radi = H/2
 count = 0
@@ -5,8 +6,8 @@ for i in range(P):
     x,y=map(int,input().split())
     if X<=x and x<=X+W and Y<=y and y<=Y+H:
         count+=1
-    elif (Math.pow((X-x),2) + Math.pow((Y+radi-y),2))<Math.pow(radi,2):
+    elif (math.pow((X-x),2) + math.pow((Y+radi-y),2))<math.pow(radi,2):
         count+=1
-    elif (Math.pow((X+W-x),2) + Math.pow((Y+radi-y),2))<Math.pow(radi,2):
+    elif (math.pow((X+W-x),2) + math.pow((Y+radi-y),2))<math.pow(radi,2):
         count+=1
 print(count)
