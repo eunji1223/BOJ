3cbe48d8a54cf77a6937aa9705d411d4b1bfffa0
eunji1223 <djer12@naver.com>
2022-06-06 22:50:59 +0900

    complete

--- 17단계. 누적 합/1.11659.py
+++ 17단계. 누적 합/1.11659.py
@@ -1,8 +1,11 @@
 N,M=map(int,input().split())
-num=list(map(int,input().split()))
+num=[0]+list(map(int,input().split()))
 for i in range(1,len(num)):
     num[i]+=num[i-1]
+print(num)
 for j in range(M):
     a,b=map(int,input().split())
-    if a-b<1: print(num[b])
-    else: print(num[b]-num[a])
+    if b-a<1:
+        print(num[b]-num[b-1])
+    else:
+        print(num[b]-num[a-1])
