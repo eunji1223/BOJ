f1e3690bc8e42a12027fbc7045d77774029a66e6
eunji1223 <djer12@naver.com>
2022-06-09 18:04:21 +0900

    complete

--- 12단계. 집합과 맵/4.10816.py
+++ 12단계. 집합과 맵/4.10816.py
@@ -1,22 +1,14 @@
-"""
-#아직 해결 X
-def binary(n,N,start,end):
-    if start>end:
-        return 0
-    m=(start+end)//2
-    if n==N[m]:
-        return N[start:end+1].counnt(n)
-    elif n<N[m]:
-        return binary(n,N,start,m-1)
-    else:
-        return binary(n,N,m+1,end)
+import sys
+input = sys.stdin.readline
 
 N=int(input())
+SN=list(map(int,input().split()))
+
 M=int(input())
-D={}
-for i in N:
-    start=0
-    end=len(N)-1
-    if i not in D:
-        D[i]=binary(n,N,start,end)
-"""
+NM=list(map(int,input().split()))
+count=[0 for _ in range(M)]
+ints=list(set(SN)&set(NM))
+for i in range(len(ints)):
+    idx=NM.index(ints[i])
+    count[idx]+=1
+print(*count)
