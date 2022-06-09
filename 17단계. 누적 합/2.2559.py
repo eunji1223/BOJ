608947e36be3980752201cb5ab0cba9dc52a2a6e
eunji1223 <djer12@naver.com>
2022-06-09 17:39:52 +0900

    complete

--- 17단계. 누적 합/2.2559.py
+++ 17단계. 누적 합/2.2559.py
@@ -3,7 +3,10 @@ input = sys.stdin.readline
 N,K=map(int,input().split())
 num=list(map(int,input().split()))
 max=0
+numS=[0]
+for i in range(1,N):
+    numS.append(numS[i-1]+num[i])
 for i in range(N-K+1):
-    if num[i]+num[i+1]>max:
-        max=num[i]+num[i+1]
+    if numS[i+K]-numS[i]>max:
+        max=numS[i+K]-numS[i]
 print(max)
