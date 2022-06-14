daadad8f16a4151cf373d30e975c9557bda1ed47
eunji1223 <djer12@naver.com>
2022-06-15 00:45:57 +0900

    complete

--- 17단계. 누적 합/3.16139.py
+++ 17단계. 누적 합/3.16139.py
@@ -12,16 +12,16 @@ Psum=[[0 for i in range(26)] for i in range(len(S))]
 
 Psum[0][ord(S[0])-97]=1
 for i in range(1,len(S)):
-    print(i)
     Psum[i][ord(S[i])-97]=1
-    for j in range(26):
+    for j in range(26): # 이전의 값을 더해줌
         Psum[i][j]+=Psum[i-1][j]
 
 for i in range(q):
     s,l,r=input().split()
-    if int(l)>0:
+    if int(l)>0: # 0보다 큰 경우
         result = Psum[int(r)][ord(s)-97]-Psum[int(l)-1][ord(s)-97]
+        # r - l까지의 l,r을 포함한 미리 구한 값으로 구간에 위치한 알파벳 값 구함
     else:
-        result=Psum[int(r)][ord(s)-97]
+        result=Psum[int(r)][ord(s)-97] # 0부터라 r까지의 값을 구함
     print(result)
     
