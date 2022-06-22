4bb72a35fc8f4cebb4389c3d90f66ff1dcec0849
eunji1223 <djer12@naver.com>
2022-06-22 23:19:38 +0900

    complete

---
+++ 17단계. 누적 합/4.10986.py
@@ -0,0 +1,15 @@
+import sys
+input=sys.stdin.readline
+
+N,M=map(int,input().split())
+A=list(map(int,input().split()))
+sumA=[0]
+for i in range(len(A)):
+    sumA.append(sumA[i]+A[i])
+
+count=0
+for i in range(len(sumA)):
+    for j in range(i+1,len(sumA)):
+        if (sumA[j]-sumA[i])%3==0:
+            count+=1
+print(count)
