c39a90ad8dc4fb8c71a23a33951440390433f4d7
eunji1223 <djer12@naver.com>
2022-06-29 00:36:48 +0900

    complete

--- 17단계. 누적 합/4.10986.py
+++ 17단계. 누적 합/4.10986.py
@@ -16,6 +16,7 @@ for i in range(len(sumA)):
 print(count)
 """ # 당연히 시간초과남
 
+"""
 import sys
 input=sys.stdin.readline
 
@@ -30,11 +31,43 @@ prefix[0]=1
 for i in range(N):
     s+=A[i]
     prefix[s%M]+=1
+print(prefix)
 # 나머지가 같은 두 부분합을 고르면 두 구간은 M의 배수가 됨
 # 나머지가 0인 경우는 부분합 자체가 M의 배수인 경우이므로 두 구간이 아닌 본인
 # 구간도 될 수 있음. INDEX가 0인(부분합 0)인 것을 포
 ans=0
 for i in prefix:
+    print(ans)
     ans+=i*(i-1)//2
 print(ans)
+"""
+import sys
+total, tar = map(int,sys.stdin.readline().split())
+
+tem = list(map(int,sys.stdin.readline().split()))
+
+prepix = [0 for i in range(tar)]
+presum = 0
+
+prepix[0] = 1
+
+
+for i in range(total):
+    presum+=tem[i]
+    # prepix.append(presum%tar)
+
+    prepix[presum%tar] += 1
+
+# print(prepix)
+'''
+나머지가 같은 두 부분합을 고르면 두 구간은 결국 tar의 배수가 된다.
+나머지가 0인 경우는 부분합 자체가 tar의 배수인 경우이므로 두 구간이 아니라 본인 구간도 될 수 있기 때문에
+index가 0인 (부분합 0) 것을 포함
+'''
+ans=0
+for i in prepix:
+    ans += i*(i-1)//2
+
+print(ans)
+
 
