0d38e128769e791c96262783f1406acdbd5c3b7f
eunji1223 <djer12@naver.com>
2022-06-30 16:03:53 +0900

    complete

--- 17단계. 누적 합/4.10986.py
+++ 17단계. 누적 합/4.10986.py
@@ -40,34 +40,26 @@ for i in prefix:
     print(ans)
     ans+=i*(i-1)//2
 print(ans)
+""" # 결과 값이 다르게 나옴
+
 """
 import sys
-total, tar = map(int,sys.stdin.readline().split())
+input=sys.stdin.readline
 
-tem = list(map(int,sys.stdin.readline().split()))
+N,M=map(int,input().split())
+A=list(map(int,input().split()))
+# N,M,*A=map(int,open(0).read().split()) 으로도 사용 가능
 
-prepix = [0 for i in range(tar)]
-presum = 0
-
-prepix[0] = 1
-
-
-for i in range(total):
-    presum+=tem[i]
-    # prepix.append(presum%tar)
-
-    prepix[presum%tar] += 1
-
-# print(prepix)
-'''
-나머지가 같은 두 부분합을 고르면 두 구간은 결국 tar의 배수가 된다.
-나머지가 0인 경우는 부분합 자체가 tar의 배수인 경우이므로 두 구간이 아니라 본인 구간도 될 수 있기 때문에
-index가 0인 (부분합 0) 것을 포함
-'''
+prefix = [0]*M
+s=0
+for i in range(N):
+    s=(s+A[i])%M
+    prefix[s]+=1
 ans=0
-for i in prepix:
-    ans += i*(i-1)//2
-
+for i in range(M):
+    if i==0: ans+=prefix[i]*(prefix[i]+1)>>1
+    else: ans+=prefix[i]*(prefix[i]+1)>>1
 print(ans)
+""" # 결과 다르게 나옴
 
 
